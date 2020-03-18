c900b1fa4f04d38a2a03a6ab0d0fa7ce88c46f20
Euler · Wong <runhub@live.com>
2020-03-18 15:27:58 +0800

    Update openni2_driver.cpp

--- src/openni2_driver.cpp
+++ src/openni2_driver.cpp
@@ -751,7 +751,7 @@ std::string OpenNI2Driver::resolveDeviceURI(const std::string& device_id) throw(
   {
       std::string s = (*available_device_URIs)[i];
       printf(s.c_str());
-      printf("\n")
+      printf("\n");
   }
   
 
